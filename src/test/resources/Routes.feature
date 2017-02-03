87c02e980a758ce4c6facb5837304175a1653b7d
dhiru30 <writetodhirendra33@gmail.com>
2017-02-03 12:24:07 +0530

    Correct the route test cases

--- src/test/resources/Routes.feature
+++ src/test/resources/Routes.feature
@@ -5,21 +5,21 @@ Feature: Routes
 
 Scenario: Check post request
   Given my server is running
-  When I hit on route post with value """post?userid=123&gender=male&nickname=Dhiru"""
+  When I hit on route post with value "post?userid=123&gender=male&nickname=Dhiru"
   Then the response should be "Accounts Created Successfully"
 
 Scenario: Check put request
 Given my server is running
-When I hit on route put with value """put?userid=123&gender=male&nickname=UpdateDhiru"""
+When I hit on route put with value "put?userid=123&gender=male&nickname=UpdateDhiru"
 Then the response should be "Accounts Update Successfully"
 
 Scenario: Check get request
 Given my server is running
-When I hit on route get with value """get?userid=123"""
+When I hit on route get with value "get?userid=123"
 Then the response should be "user  User(123,UpdateDhiru,male)"
 
 Scenario: Check delete request
 Given my server is running
-When I hit on route delete with value """delete?userid=123"""
+When I hit on route delete with value "delete?userid=123"
 Then the response should be "Accounts Delete Successfully"
 
--- src/test/scala/allTest/RouteSteps.scala
+++ src/test/scala/allTest/RouteSteps.scala
@@ -16,22 +16,22 @@ class RouteSteps extends WordSpec with ScalaDsl with EN with MockitoSugar with R
   Given("""^my server is running$"""){ () =>
   }
 
-  When("""^I hit on route post with value "([^"]*)""([^"]*)""([^"]*)"$"""){ (arg0:String, arg1:String, arg2:String) =>
-    val path = arg0+ arg1 + arg2
+  When("""^I hit on route post with value "([^"]*)"$"""){ (arg0:String) =>
+    val path = arg0
     result = Post(s"/$path") ~> allRoute
   }
 
-  When("""^I hit on route put with value "([^"]*)""([^"]*)""([^"]*)"$""") { (arg0: String, arg1: String, arg2: String) =>
-    val path = arg0+ arg1 + arg2
+  When("""^I hit on route put with value "([^"]*)"$"""){ (arg0:String) =>
+    val path = arg0
     result = Put(s"/$path") ~> allRoute
   }
 
-  When("""^I hit on route get with value "([^"]*)""([^"]*)""([^"]*)"$""") { (arg0: String, arg1: String, arg2: String) =>
-    val path = arg0+ arg1 + arg2
+  When("""^I hit on route get with value "([^"]*)"$"""){ (arg0:String) =>
+    val path = arg0
     result = Get(s"/$path") ~> allRoute
   }
-  When("""^I hit on route delete with value "([^"]*)""([^"]*)""([^"]*)"$""") { (arg0: String, arg1: String, arg2: String) =>
-    val path = arg0+ arg1 + arg2
+  When("""^I hit on route delete with value "([^"]*)"$"""){ (arg0:String) =>
+    val path = arg0
     result = Delete(s"/$path") ~> allRoute
   }
 
